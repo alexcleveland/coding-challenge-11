7797fd643329487e7f06168a3dfa350ec6da3a21
Alex Cleveland <acleveland2@usf.edu>
2024-10-26 18:00:22 -0400

    Handle order submission and display order summary

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 const productSelector = document.getElementById('product-selector');
 const quantityInput = document.getElementById('quantity');
 const totalPriceElement = document.getElementById('total-price');
-
 productSelector.addEventListener('change', updateTotalPrice);
 quantityInput.addEventListener('input', updateTotalPrice);
 
@@ -13,3 +12,28 @@ function updateTotalPrice() {
     const totalPrice = productPrice * quantity;
       totalPriceElement.textContent = totalPrice.toFixed(2);
 }
+// this is the alert for invalid order.
+function orderAlert() {
+    const quantity = parseInt(quantityInput.value);
+    if (quantity < 1) {
+        alert(' Enter a valid quantity please!!.');
+        return false;
+    }
+    return true;
+}
+
+// Task 4 Handle Order Submission
+
+const placeOrderButton = document.getElementById('place-order');
+const orderSummary = document.getElementById('order-summary');
+
+placeOrderButton.addEventListener('click', function() {
+    if (!orderAlert())
+     return;
+
+    const selectedProduct = productSelector.options[productSelector.selectedIndex].text;
+    const quantity = quantityInput.value;
+    const totalPrice = totalPriceElement.textContent;
+    
+    orderSummary.textContent = `Thank you for ordering ${quantity} of ${selectedProduct}. Your Total today is $${totalPrice} Thank you for shopping at Bürobedarf!!!! `;
+});
