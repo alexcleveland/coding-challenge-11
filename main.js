bacd84d51c10eaa18f59e8d016befeb363e13026
Alex Cleveland <acleveland2@usf.edu>
2024-10-26 17:32:00 -0400

    Calculate and update total price dynamically based on product and quantity

--- main.js
+++ main.js
@@ -6,3 +6,10 @@ const totalPriceElement = document.getElementById('total-price');
 productSelector.addEventListener('change', updateTotalPrice);
 quantityInput.addEventListener('input', updateTotalPrice);
 
+// Task 3 Calculate and update total price dynamically based on product and quantity
+function updateTotalPrice() {
+    const productPrice = parseFloat(productSelector.value);
+    const quantity = parseInt(quantityInput.value);
+    const totalPrice = productPrice * quantity;
+      totalPriceElement.textContent = totalPrice.toFixed(2);
+}
